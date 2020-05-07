b0fa59eaafd4dd23c918e34850323e6d5fd925f7
ricardoalvesra <ricardoalves@live.com>
2020-05-07 16:32:45 +0100

    project1 done

--- project1/README.md
+++ project1/README.md
@@ -1,3 +1,20 @@
 # Project 1
 
 Web Programming with Python and JavaScript
+
+# import.py
+
+Import csv file in database
+
+# application.py
+
+Main website python file
+
+# templates
+
+HTML templates used in website. 
+All templates extends from layout.html.
+
+# static/css
+
+Main stylesheet file
--- project1/application.py
+++ project1/application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask, session, request, render_template, jsonify 
+from flask import Flask, session, request, render_template, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -24,32 +24,33 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    logged = "username" in session
-    return render_template("index.html", logged=logged)
+    return render_template("index.html")
 
 
 @app.route("/signin")
-def open_signin():
+def get_signin():
     return render_template("signin.html")
 
 
 @app.route("/signin", methods=["POST"])
-def signin():
+def post_signin():
     username = request.form.get("username")
     password = request.form.get("password")
 
     if db.execute("SELECT * FROM users WHERE username = :username and password = :password", {"username": username, "password": password}).rowcount != 0:
         session["username"] = username
-        return render_template("index.html", logged=True)
+        return render_template("index.html")
+
+    return render_template("error.html", message="User not found!")
 
 
 @app.route("/register")
-def open_register():
+def get_register():
     return render_template("register.html")
 
 
 @app.route("/register", methods=["POST"])
-def register():
+def post_register():
     username = request.form.get("username")
     password = request.form.get("password")
 
@@ -59,17 +60,10 @@ def register():
     db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
                {"username": username, "password": password})
     db.commit()
-
+    
     session["username"] = username
 
-    return render_template("index.html", logged=True)
-
-
-@app.route("/api")
-def api():
-    response = requests.get("https://www.goodreads.com/book/review_counts.json",
-                            params={"key": "MuE9uBE81jrQ2sdmLfEMMg", "isbns": "9781632168146"})
-    return response.json()
+    return render_template("index.html")
 
 
 @app.route("/logout")
@@ -80,29 +74,31 @@ def logout():
 
 @app.route("/search", methods=["POST", "GET"])
 def search():
-    logged = "username" in session
-    text = request.form.get("text")
-    if text is None:
-        return render_template("search.html", logged=logged)
-    else:
-        books = db.execute("SELECT isbn, title, author, year FROM books WHERE lower(isbn) LIKE :text OR lower(title) LIKE :text OR lower(author) LIKE :text",
+    if request.method == 'POST':
+        text = request.form.get("text")
+        books = db.execute("SELECT isbn, title, author, year FROM books WHERE lower(isbn) LIKE :text OR lower(title) LIKE :text OR lower(author) LIKE :text order by title limit 100",
                            {"text": '%'+text.lower()+'%'}).fetchall()
 
-        return render_template("search.html", books=books, logged=logged)
+        message = ""
+        if len(books) == 0:
+            message = "Book not found."
 
-
+        return render_template("search.html", books=books, message=message)
+    else:
+        return render_template("search.html")
+    
 @app.route("/book/<isbn>", methods=["POST", "GET"])
 def book(isbn):
-
-    username = session["username"]
-    logged = "username" in session
-
     if request.method == 'POST':
+
+        if request.form.get("star") is None:
+            return render_template("error.html", message="You must provide a!")
+
+        username = session["username"]
         if db.execute("SELECT * FROM reviews WHERE username = :username and isbn = :isbn", {"username": username, "isbn": isbn}).rowcount != 0:
             return render_template("error.html", message="You can make only one review per book.")
 
-        username = session["username"]
-        rating = request.form.get("rating")
+        rating = request.form.get("star")
         description = request.form.get("description")
 
         db.execute("INSERT INTO reviews (username, isbn, description, rating) VALUES (:username, :isbn, :description, :rating)", {"username": username, "isbn": isbn, "description": description, "rating": rating}
@@ -115,23 +111,26 @@ def book(isbn):
     reviews = db.execute("SELECT description, rating, username, isbn FROM reviews WHERE lower(isbn) LIKE :text", {
                          "text": '%'+isbn.lower()+'%'}).fetchall()
 
-    response = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "MuE9uBE81jrQ2sdmLfEMMg", "isbns": isbn})
-    goodreads = response.json()["books"][0]
-    print(goodreads)
-
-    return render_template("book.html", book=book, logged=logged, reviews=reviews, goodreads=goodreads)
+    return render_template("book.html", book=book, reviews=reviews, goodreads=get_goodread_rating())
 
 
 @app.route("/api/<isbn>")
 def bookapi(isbn):
-
     book = db.execute("select count(1), avg(rating), title, author, year, books.isbn from books, reviews where books.isbn = reviews.isbn and lower(books.isbn) LIKE :text group by title, author, year, books.isbn  ", {
         "text": '%' + isbn.lower() + '%'}).fetchone()
 
-    return jsonify({"title": book.title,
+    if book is None:
+        return jsonify({"status": 404, "message": "book not found"})
+    else:
+        return jsonify({"title": book.title,
                     "author": book.author,
                     "year": book.year,
                     "isbn": book.isbn,
                     "review_count": book.count,
                     "average_score": str(book.avg)
                     })
+
+def get_goodread_rating():
+    response = requests.get("https://www.goodreads.com/book/review_counts.json",
+                            params={"key": "MuE9uBE81jrQ2sdmLfEMMg", "isbns": isbn})
+    return response.json()["books"][0]
