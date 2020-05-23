de0db996d4072ef8ba6f55890579799d9a85a697
ricardoalvesra <ricardoalves@live.com>
2020-05-23 03:16:36 +0100

    project 2

--- project2/static/index.js
+++ project2/static/index.js
@@ -13,14 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         socket.emit('send message', { 'channel': '', 'message': '' });
     });
 
-    // When a new vote is announced, add to the unordered list
     socket.on('channels', data => {
-        // if (data.channelconnected == connectedChannel()) {
-        // var connectedChannel = 0
-        // var items = document.querySelectorAll(".btn-outline-success");
-        // if (items.length > 0)
-        //     connectedChannel = parseInt(items[0].dataset.code);
-
         document.querySelector('#channels').innerHTML = '';
 
         data.forEach(element => {
@@ -46,15 +39,6 @@ document.addEventListener('DOMContentLoaded', function () {
             document.querySelector("#channel_" + currentChannel).className = "btn btn-outline-success";
         else
             document.querySelectorAll("[id^='channel_']")[0].className = "btn btn-outline-success";
-
-
-        // if (connectedChannel != 0)
-        //     document.querySelectorAll(".btn.btn-sm.btn-outline-secondary")[connectedChannel].className = "btn btn-outline-success";
-
-        // let items2 = document.querySelectorAll(".btn-outline-success");
-        // if (items2.length == 0)
-        //     document.querySelectorAll(".btn.btn-sm.btn-outline-secondary")[0].className = "btn btn-outline-success";
-
     });
 
     socket.on('messages', data => {
